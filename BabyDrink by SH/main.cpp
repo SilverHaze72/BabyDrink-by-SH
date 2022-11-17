0f7aa5a14ac36c16f7047db4de155da319ff469f
SilverHaze <titouan.bourion@gmail.com>
2022-11-17 22:44:03 +0100

    Commentaire add

--- BabyDrink by SH/main.cpp
+++ BabyDrink by SH/main.cpp
@@ -1,4 +1,12 @@
-﻿//#include "SDL.h"
+﻿//////////////////////////
+//						//
+//      Baby drink		//
+//	by BOURION TiTouan  //
+//   aka Silver_Haze_   //
+//						//
+//////////////////////////
+
+//#include "SDL.h"
 //#include "SDL_image.h"
 #undef main
 
@@ -12,59 +20,58 @@
 #include <vector>
 using namespace std;
 #include "baby.h"
-#include "shopping_List.h"
 #include <list>
 
 
 /*
-//Screen dimension constants
+//dimension de la fenêtre
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-//Starts up SDL and creates window
+//Start SDL et creer la fenêtre
 bool init();
 
-//Frees media and shuts down SDL
+//shuts down SDL
 void close();
 
-//The window we'll be rendering to
+//Mise en place de la fenêtre
 SDL_Window* gWindow = NULL;
 
-//The surface contained by the window
+//Suface contenu dans la fenêtre
 SDL_Surface* gScreenSurface = NULL;
 
-//The window renderer
+//Rendu de la fenêtre
 SDL_Renderer* gRenderer = NULL;
 
 bool init()
 {
-	//Initialization flag
+	//Init flag "sucess"
 	bool success = true;
 
-	//Initialize SDL
+	//Init SDL
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
-		cout << "SDL could not initialize! SDL_Error: %s" << endl << SDL_GetError();
+		cout << "SDL ne peut etre initialisé ! SDL_Error: %s" << endl << SDL_GetError();
 		success = false;
 	}
 	else
 	{
-		//Create window
-		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		//creer la fenêtre
+		gWindow = SDL_CreateWindow("Baby Drink", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 		if (gWindow == NULL)
 		{
-			cout << "Window could not be created! SDL_Error: %s" << endl << SDL_GetError();
+			cout << "Window ne peut etre cree ! SDL_Error: %s" << endl << SDL_GetError();
 			success = false;
 		}
 		else
 		{
-			//Get window surface
+			//recupère la surface
 			gScreenSurface = SDL_GetWindowSurface(gWindow);
 
-			//Fill the surface white
+			//Remplis la surface en blanc
 			SDL_FillRect(gScreenSurface, NULL, SDL_MapRGB(gScreenSurface->format, 130, 220, 255));
 
-			//Update the surface
+			//Met à jour la surface
 			SDL_UpdateWindowSurface(gWindow);
 		}
 	}
@@ -74,24 +81,24 @@ bool init()
 
 bool loadMedia()
 {
-	//Loading success flag
+	//Load le flag "sucess
 	bool success = true;
 
-	//Nothing to load
 	return success;
 }
 
 void close()
 {
-	//Destroy window
+	//Destruit window
 	SDL_DestroyWindow(gWindow);
 	gWindow = NULL;
 
-	//Quit SDL subsystems
+	//Quitte les sous-systèmes SDL
 	SDL_Quit();
 }
 */
 
+//Clear console
 void clrscr()
 {
 	system("cls");
@@ -101,7 +108,7 @@ int main(int argc, char* args[])
 {
 
 	/*
-	//Start up SDL and create window
+	//Start SDL et creer la fenêtre
 	if (!init())
 	{
 		cout << "Failed to initialize!" << endl;
@@ -115,19 +122,19 @@ int main(int argc, char* args[])
 		}
 		else
 		{
-			//Main loop flag
+			//flag de la boucle principale
 			bool quit = false;
 
 			//Event handler
 			SDL_Event e;
 
-			//While application is running
+			//tant que l'application est ON
 			while (!quit)
 			{
-				//Handle events on queue
+				//Traitement des events de la file d'attente
 				while (SDL_PollEvent(&e) != 0)
 				{
-					//User requests quit
+					//Requête utilisateur : quitter
 					if (e.type == SDL_QUIT)
 					{
 						quit = true;
@@ -138,7 +145,7 @@ int main(int argc, char* args[])
 				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 				SDL_RenderClear(gRenderer);
 
-				//Render red filled quad
+				//Test de rendu non fonctionnel (mis en attente)
 				SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
 				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
 				SDL_RenderFillRect(gRenderer, &fillRect);
@@ -149,20 +156,21 @@ int main(int argc, char* args[])
 		}
 	}
 
-	//Free resources and close SDL
+	//close SDL et sous-systèmes
 	close();
 
 	*/
 
+	//Initalisation des variables
 	string ingredient;
 	int laitTot, laitRest, drinkStat, regStat, quantIngr;
 	int choiceMenu;
 	int newTimeAlert, hours, minutes;
 	bool drink = true, regur = true;
-	string n;
-	int q;
 
+	//label Start
 	start:
+		//Interface console (Main Menu)
 		cout << " ---------------------" << endl;
 		cout << "||                   ||" << endl;
 		cout << "||     Baby Drink    ||" << endl;
@@ -174,14 +182,17 @@ int main(int argc, char* args[])
 		cout << "2 - Ajouter une alerte" << endl;
 		cout << "3 - Liste de course" << endl;
 		cout << "4 - Quitter" << endl << endl;
+		//Choix menu
 		cout << "Choix: ";
 		cin >> choiceMenu;
 
+		//verif de choix valide
 	if (choiceMenu >= 1 && choiceMenu <= 4) {
-		
+		//traitement du choix pour redirection
 		switch (choiceMenu) {
 
 			case 1:
+				//clear console et creation biberon
 				clrscr();
 
 				biberon bib;
@@ -190,18 +201,20 @@ int main(int argc, char* args[])
 				cin >> drinkStat;
 				cout << "entrer la quantite de lait dans le biberon (en cl) : ";
 				cin >> laitTot;
-
+				//verif biberon bu et redirection sur la suite d'entrer des données liées au biberon
 				if (drinkStat == 2) {
 					laitRest = laitTot;
 					regur = false;
 				}
-				
-				cout << "entrer la quantite restante dans le biberon (en cl) : ";
-				cin >> laitRest;
-				cout << " Le bebe a t-il regurgite ?" << endl << "1 - oui" << endl << "2 - non" << endl << "Reponse : ";
-				cin >> regStat;
-				cout << endl;
-
+				else
+				{
+					cout << "entrer la quantite restante dans le biberon (en cl) : ";
+					cin >> laitRest;
+					cout << " Le bebe a t-il regurgite ?" << endl << "1 - oui" << endl << "2 - non" << endl << "Reponse : ";
+					cin >> regStat;
+					cout << endl;
+				}
+				//traitement des données pour les rendrent lisible par la classe
 				if (drinkStat == 1)
 					drink = true;
 				else if (drinkStat == 2)
@@ -211,15 +224,17 @@ int main(int argc, char* args[])
 					regur = true;
 				else if (regStat == 2)
 					regur = false;
-
+				//redirection des données vers la classe
 				bib.biberonStatut(drink, laitTot, laitTot - laitRest, regur);
 				break;
 
 			case 2:
+				//clear console et creation de l'alert
 				clrscr();
 
 				alert newAlert;
 
+				//recuperation du temps avant l'alarme defini par l'utilisateur
 				cout << "Dans combien de temps voulez vous votre prochaine alert ?" << endl;
 				cout << "Nombre d'heures : ";
 				cin >> hours;
@@ -227,38 +242,34 @@ int main(int argc, char* args[])
 				retryMinutes:
 					cout << "Nombre de minutes : ";
 					cin >> minutes;
-
+					//verif de données valide
 					if (minutes < 0 || minutes > 59){
 						cout << "Error : minutes are invalid, please enter valid values between 0 and 59." << endl;
 						goto retryMinutes;
 					}
-
+					//redirection des données vers la classe
 				newTimeAlert = hours * 3600000 + minutes * 60000;
 				newAlert.addalert(newTimeAlert);
 				break;
 
 			case 3:
-				system("exit");
-				return 0;
-				break;
-				
+				//Liste de course
+				cout << "entrer le nom d'un ingredient : ";
+				cin >> ingredient;
+				cout << "entrer la quatité de " << ingredient << " pour un biberon : ";
+				cin >> quantIngr;
+
+				course produit1;
+				produit1.addProduit(ingredient, quantIngr);
+
 			case 4:
+				//solution exit systeme
 				system("exit");
 				return 0;
 				break;
 		}
 		goto start;
 	}
-
-	//Liste de course
-	cout << "entrer le nom d'un ingredient : ";
-	cin >> ingredient;
-	cout << "entrer la quatité de " << ingredient << " pour un biberon : ";
-	cin >> quantIngr;
-
-	course produit1;
-	produit1.addProduit(ingredient, quantIngr);
-
 
 	return 0;
 }
